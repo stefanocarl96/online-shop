399a7936018256ff3b787c65494837590d10b92a
fmaylinch <ferranmaylinch@gmail.com>
2018-10-17 10:05:48 +0200

    Create Product class

---
+++ src/main/java/tech/bts/onlineshop/Product.java
@@ -0,0 +1,26 @@
+package tech.bts.onlineshop;
+
+public class Product {
+
+    private String name;
+    private double price;
+    private boolean available;
+
+    public Product(String name, double price) {
+        this.name = name;
+        this.price = price;
+        this.available = true;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public boolean isAvailable() {
+        return available;
+    }
+}
