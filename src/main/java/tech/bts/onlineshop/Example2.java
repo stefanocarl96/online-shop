8bf21cc8768068abf69d02fb087919fa9d6ee765
ASUS <stefanocarluccio@live.it>
2018-10-25 18:30:41 +0200

    Create ControlAvailability and PossibletoDeliver method

--- src/main/java/tech/bts/onlineshop/Example2.java
+++ src/main/java/tech/bts/onlineshop/Example2.java
@@ -1,20 +1,34 @@
 package tech.bts.onlineshop;
 
-import tech.bts.onlineshop.business.Product;
 import tech.bts.onlineshop.business.ProductService;
 import tech.bts.onlineshop.data.ProductDatabase;
+import tech.bts.onlineshop.model.Product;
 
 public class Example2 {
 
-    public static void main(String args) {
+    public static void main(String[] args) {
 
         ProductDatabase productDatabase = new ProductDatabase();
+        ProductService p1 = new ProductService(productDatabase);
 
+        long macBookId = p1.createProduct(new Product("MacBook", "Apple", 1400));
+        long iphoneID = p1.createProduct(new Product("iPhone xs", "Apple", 1000));
 
+        p1.addProductStock(macBookId,200);
+        p1.addProductStock(iphoneID, 300);
+        p1.addProductStock(macBookId,100);
 
-        ProductService productService = new ProductService();
-        productService.createProduct(new product("Iphone XS", "apple", 1250));
-        productService.createProduct(new Product("Pixel 3", "Google", 900));
+        long requestedId = macBookId;
+        Product requestedProduct = p1.getById(requestedId);
+        System.out.println("There are " + requestedProduct.getQuantity() + " units of " + requestedProduct.getName() + " in stock");
 
+        int requestedQuantity = 20;
+        boolean availability = p1.controlAvailability(requestedId,requestedQuantity);
+        System.out.println(availability);
 
+        System.out.println(p1.possibletoDeliver(requestedId,requestedQuantity));
+
+    }
 }
+
+
--- src/main/java/tech/bts/onlineshop/business/ProductService.java
+++ src/main/java/tech/bts/onlineshop/business/ProductService.java
@@ -2,39 +2,51 @@ package tech.bts.onlineshop.business;
 
 import tech.bts.onlineshop.data.ProductDatabase;
 import tech.bts.onlineshop.model.CartItem;
+import tech.bts.onlineshop.model.Product;
 import tech.bts.onlineshop.model.ShoppingCart;
 
 public class ProductService {
 
-    public long getName;
     private ProductDatabase productDatabase;
-
 
     public ProductService(ProductDatabase productDatabase) {
         this.productDatabase = productDatabase;
     }
 
-    public Product getProductById(long productid) {
-        Product p = productDatabase.get(productid);
-        return p;
-
-    }
-
-
     public long createProduct(Product product) {
-       return this.productDatabase.add(product);
+        return this.productDatabase.add(product);
     }
-
 
     public void addProductStock(long productId, int quantity) {
-
         Product product = this.productDatabase.get(productId);
         int total = product.getQuantity() + quantity;
         product.setQuantity(total);
     }
 
+    public Product getById(long productId) {
 
-    public int getCount() {
-        return 0;
+        Product product = this.productDatabase.get(productId);
+        return product;
     }
+
+    public boolean controlAvailability(long productId, int quantity) {
+
+        Product product = this.productDatabase.get(productId);
+        if (quantity <= product.getQuantity()) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public int possibletoDeliver(long productId, int quantity) {
+
+        Product product = this.productDatabase.get(productId);
+        if (quantity <= product.getQuantity()) {
+            return quantity;
+        } else {
+            return product.getQuantity();
+        }
+    }
+
 }
